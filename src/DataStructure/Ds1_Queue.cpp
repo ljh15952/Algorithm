911801888ad46c35e8ff2c5127b76b510e1247a3
중화이 <ljh15952@gmail.com>
2022-05-11 10:18:47 +0000

    5/11 19:18 Queue

--- src/DataStructure/Ds1_Queue.cpp
+++ src/DataStructure/Ds1_Queue.cpp
@@ -2,13 +2,52 @@
 
 using namespace std;
 
+//연결 리스트로 구현 한 큐 자료구조
+
+class Node{
+	public:
+		Node(int i) : data(i){};
+		int data;
+		Node * next;
+};
+
+class Queue{
+	public:
+		void Enqueue(int i){
+			Node *n = new Node(i);
+			if(front == nullptr){
+				front = n;
+				back = front;
+			}else{
+				back->next = n;
+				back = back->next;
+			}
+		}
+		int Dequeue(){
+			int res = front->data;
+			front = front->next;
+			return res;
+		}
+		
+		Node *front = nullptr;
+		Node *back = nullptr;
+};
+
 int main(){
 	
 	cout << "queue study" << endl;
 	
-	/*
+	Queue *queue = new Queue();
+	queue->Enqueue(1);
+	queue->Enqueue(2);
+	queue->Enqueue(3);
+	queue->Enqueue(4);
+	queue->Enqueue(5);
+
+	for(int i = 0; i < 5; ++i){
+		cout << queue->Dequeue() << endl;
+	}
 	
 	
-	*/
 	return 0;
 }
