31841bb3f2cc5152a3956727bd20e04fe90c4469
중화이 <ljh15952@gmail.com>
2022-07-01 11:05:09 +0000

    7/1 20:05

--- src/Graph/graph.cpp
+++ src/Graph/graph.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 
+bool visited[101]; // 방문 여부를 체크하는 배열
+
+void dfs(int k){
+	for(int i = 0; i < G[i].size(); ++i){
+		if(!visited[G[K][i].to]){
+			visited[G[k][i].to] = true;
+			dfs(G[k][i]);
+		}
+	}
+	return;
+}
+
 int main(){
 	
 	cout << "graph" << endl;
