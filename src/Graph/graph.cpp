bb1b1b16172fd81eaa97eca532f948e7112b2a8d
중화이 <ljh15952@gmail.com>
2022-07-01 11:19:45 +0000

    7/1 20:19

--- src/Graph/graph.cpp
+++ src/Graph/graph.cpp
@@ -1,21 +1,39 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 bool visited[101]; // 방문 여부를 체크하는 배열
 
-void dfs(int k){
-	for(int i = 0; i < G[i].size(); ++i){
-		if(!visited[G[K][i].to]){
-			visited[G[k][i].to] = true;
-			dfs(G[k][i]);
-		}
-	}
-	return;
-}
+class Edge{
+	public:
+		Edge(int s,int e) : snode(s), enode(e) {};
+		int snode;
+		int enode;
+};
+
+// void dfs(int k){
+// 	for(int i = 0; i < G[i].size(); ++i){
+// 		if(!visited[G[K][i].to]){
+// 			visited[G[k][i].to] = true;
+// 			dfs(G[k][i]);
+// 		}
+// 	}
+// 	return;
+// }
 
 int main(){
 	
+	Edge e1(1,2);
+	Edge e2(2,3);
+	Edge e3(3,4);
+	Edge e4(3,7);
+	Edge e5(3,5);
+	Edge e6(2,6);
+	Edge e7(6,8);
+	Edge e8(1,8);
+	Edge e9(1,9);
+
 	cout << "graph" << endl;
 	
 	return 0;
