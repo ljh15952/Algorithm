82058154ec683107635d770a26757027e4a7bf71
중화이 <ljh15952@gmail.com>
2022-07-03 07:03:18 +0000

    7/3 16:03

--- src/Graph/graph.cpp
+++ src/Graph/graph.cpp
@@ -3,16 +3,17 @@
 
 using namespace std;
 
-bool visited[9]; // 방문 여부를 체크하는 배열
 vector<int> graph[9];
+bool visited[9];
 
-void dfs(int x){
-	visited[x] = true;
-	cout << x << " ";
-	for(int i = 0; i < graph[x].size(); ++i){
-		int y = graph[x][i];
-		if(!visited[y])
-			dfs(y);
+void dfs(int n){
+	cout << n << " ";
+	visited[n] = true;
+	
+	for(int i = 0; i < graph[n].size(); ++i){
+		int k = graph[n][i];
+		if(visited[k] == false)
+			dfs(k);
 	}
 }
 
@@ -21,34 +22,31 @@ int main(){
 	graph[1].push_back(2);
 	graph[1].push_back(3);
 	graph[1].push_back(8);
-
+	
 	graph[2].push_back(1);
 	graph[2].push_back(7);
 	
 	graph[3].push_back(1);
 	graph[3].push_back(4);
 	graph[3].push_back(5);
-
+	
 	graph[4].push_back(3);
 	graph[4].push_back(5);
 	
 	graph[5].push_back(3);
 	graph[5].push_back(4);
 	
+	
 	graph[6].push_back(7);
-
+	
 	graph[7].push_back(2);
 	graph[7].push_back(6);
 	graph[7].push_back(8);
-	
+
 	graph[8].push_back(1);
 	graph[8].push_back(7);
-
-
-	dfs(1);
 	
-	//1 2 3 4 5 7 6 8 9
-	cout << "graph" << endl;
+	dfs(1);
 	
 	return 0;
 }
