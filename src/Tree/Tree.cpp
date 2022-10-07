09106192b25ba036e2d19af1ae607e181663abb6
중화이 <ljh15952@gmail.com>
2022-10-07 11:50:59 +0000

    10/7 20:50

--- src/Tree/Tree.cpp
+++ src/Tree/Tree.cpp
@@ -2,24 +2,43 @@
 
 using namespace std;
 
-struct Node;
+class Node;
 
-struct List{
-	struct List *next;
-	struct Node *element;
+class List{
+	public:
+		List(int a){
+			element = new Node(a);
+		}
+		List * next;
+		Node * element;
+	
+		void createNextList(int a){
+			next = new List(a);
+		}
 };
 
-struct Node{
-	struct Node * Parent;
-	struct List * Chlidren;
+class Node{
+	public:
+		Node(int v) : value(v) {};
+		Node * parent;
+		List * chlidren; // 자식의 마지막을 가르키 도록..
+	
+		int value;	
+	
+		void createChild(int a){
+			if(chlidren){
+				// 만약 이미 만들었다면
+				chlidren->createNextNode(a);
+			}else{
+				children = new List(a);
+			}
+		}
 };
 
 int main(){
 	
-	Node * root;
+	Node * root = new Node(1);
 	
-	
+	// 좀 더 고민.. 어쨋든 연결리스트 로 이루오 지면 된다는거 아녀
 	return 0;
 }
-// next study
-//
