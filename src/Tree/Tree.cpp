5171ef33327ff20844c80a882bd9ba1c01abbd7b
중화이 <ljh15952@gmail.com>
2022-10-08 05:31:32 +0000

    10/8 14:31

--- src/Tree/Tree.cpp
+++ src/Tree/Tree.cpp
@@ -1,44 +1,29 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
-
-class Node;
-
-class List{
-	public:
-		List(int a){
-			element = new Node(a);
-		}
-		List * next;
-		Node * element;
-	
-		void createNextList(int a){
-			next = new List(a);
-		}
-};
 
 class Node{
 	public:
 		Node(int v) : value(v) {};
 		Node * parent;
-		List * chlidren; // 자식의 마지막을 가르키 도록..
+		vector<Node*> children;
 	
 		int value;	
 	
-		void createChild(int a){
-			if(chlidren){
-				// 만약 이미 만들었다면
-				chlidren->createNextNode(a);
-			}else{
-				children = new List(a);
-			}
+		void addChild(Node * n){
+			children.push_back(n);
 		}
 };
 
 int main(){
 	
-	Node * root = new Node(1);
+	Node * one = new Node(1);
 	
-	// 좀 더 고민.. 어쨋든 연결리스트 로 이루오 지면 된다는거 아녀
+	Node * two = new Node(2);
+	two->parent = one;
+	
+	one->addChild(two);
+	
 	return 0;
 }
