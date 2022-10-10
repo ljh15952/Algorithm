d10c97588f877d256863a011e71e7084d5e20aa2
중화이 <ljh15952@gmail.com>
2022-10-10 11:22:10 +0000

    10/10 Binary Search Tree

--- src/Tree/Binary_searchTree.cpp
+++ src/Tree/Binary_searchTree.cpp
@@ -49,13 +49,35 @@ class BST{
 					curNode->right = new Node(v);
 			}
 		}
-	
+		
 		//삭제는 조금 어려우니 다음번에 할게요
+	
+		void traverse(Node * curNode){
+			if(curNode == nullptr)
+				return;
+			traverse(curNode->left);
+			cout << curNode->value << "-> ";
+			traverse(curNode->right);
+		}
 };
 
 int main(){
 	
+	BST * tree = new BST();
+	//루트 선정을 잘 해야 함
+	tree->setRoot(new Node(7));
 	
+	
+	tree->insertNode(tree->root, 3);
+	tree->insertNode(tree->root, 1);
+	tree->insertNode(tree->root, 5);
+	tree->insertNode(tree->root, 8);
+	tree->insertNode(tree->root, 10);
+	
+	tree->traverse(tree->root);
+	cout << endl;
+	
+	cout << tree->findNode(tree->root, 5) << endl;
 	
 	return 0;
 }
